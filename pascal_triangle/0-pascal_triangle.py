db433340cfb3024dbf1085ab56cee8314d29e7b5
EvaGLN <maeva.galiano@gmail.com>
2026-09-11 11:20:25 +0200

    Add 0-pascal-triangle.py

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1]
+        for j in range(1, i):
+            result = triangle[i-1][j-1] + triangle[i-1][j]
+            row.append(result)
+        if i > 0:
+            row.append(1)
+
+        triangle.append(row)
+
+    return triangle
